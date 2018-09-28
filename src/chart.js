73d34dada79acfccb739b940400a58146b6f6de6
rodoabad <rodoabad@gmail.com>
2018-09-27 22:26:01 -0500

    test: test `100%1 per @suddencreator

--- src/chart.js
+++ src/chart.js
@@ -15,17 +15,14 @@ const data = [
 export const SimpleAreaChart = () => {
   return (
     <section className={chart}>
-      <ResponsiveContainer
-        aspect={1}
-        width={'99%'}
-      >
+      <ResponsiveContainer aspect={1}>
         <AreaChart data={data}
           margin={{top: 10, right: 30, left: 0, bottom: 0}}>
-          <CartesianGrid strokeDasharray="3 3" />
-          <XAxis dataKey="name" />
+          <CartesianGrid strokeDasharray='3 3' />
+          <XAxis dataKey='name' />
           <YAxis />
           <Tooltip />
-          <Area type='monotone' dataKey='uv' stroke='#8884d8' fill='#8884d8' />
+          <Area type='monotone' dataKey='uv' stroke='#BD3D3A' fill='#BD3D3A' />
         </AreaChart>
       </ResponsiveContainer>
     </section>
