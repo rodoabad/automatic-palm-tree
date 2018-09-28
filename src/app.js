7d4ef04b1a64f86af8c3f5860302acf09e4056f4
rodoabad <rodoabad@gmail.com>
2018-09-27 22:07:02 -0500

    chore: add `recharts` to test grid problem

--- src/app.js
+++ src/app.js
@@ -3,6 +3,10 @@ import React from 'react';
 import {RouteConfig} from './route-config';
 import {hot} from 'react-hot-loader';
 
-export const App = () => <RouteConfig/>;
+export const App = () => (
+  <React.Fragment>
+    <RouteConfig />
+  </React.Fragment>
+);
 
 export const AppWithHotReload = hot(module)(App);
--- src/home.js
+++ src/home.js
@@ -1,10 +1,12 @@
 import {Hello} from './hello';
 import PropTypes from 'prop-types';
 import React from 'react';
+import {SimpleAreaChart} from './chart';
 
 export const Home = ({className}) => (
   <section className={className}>
     <Hello />
+    <SimpleAreaChart/>
   </section>
 );
 
